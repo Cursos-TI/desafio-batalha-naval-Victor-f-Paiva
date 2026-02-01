9ebb7bbcde986ed54ec32f16352c6e0f3cad4bbd
Victor Paiva <victor_eduardof@hotmail.com>
2026-02-01 12:52:10 +0000

    feat: Nível Aventureiro. Add dois navios na diagonal. Add validação para saber imprimir mensagem caso exista peça na posição e interrupção do loop. Add validação com mensagem de aviso caso a posição exceda o tamanho do tabuleiro com interrupção do loop

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,20 +1,100 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAMANHOTABULEIRO 10
+#define TAMANHONAVIO 3
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    //1- cabeçalho do tabuleiro
+    char cabeçalho[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
+    //2- criando o tabuleiro
+    int tabuleiro[10][10];
+    for (int i = 0; i < TAMANHOTABULEIRO; i++)
+    {
+        for (int j=0; j< TAMANHOTABULEIRO; j++){
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    //3- incluindo navios vertical e horizontal no tabuleiro
+    //3.1- navio vertical em B3-B4-B5 {(2,1), (3,1), (4,1)}
+    for (int i = 0; i < TAMANHONAVIO; i++)
+    {
+        tabuleiro[i+2][1] = 3;
+    }
+    
+    //3.2- navio horizontal em C5{(5,2), (5,3), (5,4)}
+    for (int i = 0; i < TAMANHONAVIO; i++)
+    {
+        tabuleiro[4][i+2] = 3;
+    }
+    
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // 1- incluindo dois navios na diagonal
+    //1.1- navio diagonal1 em F2-G3-H4 {(1,5), (2,6), (3,7)}
+    for (int i=0; i< TAMANHONAVIO; i++)
+    {
+        //1.1.1- criando validação para imprimir mensagem caso exista peça !=0 na posição
+        if (tabuleiro[i+1][i+5] != 0)
+        {
+            printf("Já existe uma peça na linha %d, coluna %d \n", i+1, i+5);
+            break;
+        }
+        //1.1.2- incluindo validação para saber se peça está dentro dos limites do tabuleiro
+        else if (i+1 > TAMANHOTABULEIRO || 5+i > TAMANHOTABULEIRO)
+        {
+            printf("Linha %d, coluna %d excede o tamnaho do tabuleiro. \n", i+1, i+5);
+            break;
+        }
+        //1.1.3- inclusão do navio caso não tenha peça na posição
+        else {
+            tabuleiro[i+1][i+5] = 3;
+        }
+    }
+    
+    //1.2- navio diagonal2 em F9-G8-H7 {(8,5), (7,6), (6,7)}
+    for (int i=0; i< TAMANHONAVIO; i++)
+    {
+        //1.2.1- criando validação para imprimir mensagem caso exista peça !=0 na posição
+        if (tabuleiro[8-i][5+i] != 0)
+        {
+            printf("Já existe uma peça na linha %d, coluna %d \n", 8-i, 5+i);
+            break;
+        }
+        //1.2.2- incluindo validação para saber se peça está dentro dos limites do tabuleiro
+        else if (8-i > TAMANHOTABULEIRO || 5+i > TAMANHOTABULEIRO)
+        {
+            printf("Linha %d, coluna %d excede o tamnaho do tabuleiro. \n", 8-i, 5+i);
+            break;
+        }
+        //1.2.3- inclusão do navio caso não tenha peça na posição
+        else {
+            tabuleiro[8-i][5+i] = 3;
+        }
+    }
+     
+    //2.1- imprimindo cabeçalho
+    printf("-- ");
+    for (int i=0; i<TAMANHOTABULEIRO; i++)
+    {
+        printf("%c ", cabeçalho[i]);
+    }
+    printf("\n");
 
+    //2.2- imiprimindo tabuleiro
+    for (int i=0; i<TAMANHOTABULEIRO; i++)
+    {
+        //if-else para que a numeração das linhas fique estetica, com 2 espaços de 1-9 e um espaço na linha 10
+        if(i==9){ printf("%d ", i+1);}
+        else {printf("%d  ", i+1);}
+        for (int j=0; j<TAMANHOTABULEIRO; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
