5e1079539fef1ca31e5d377f81b1e7f6efef9f28
Victor Paiva <victor_eduardof@hotmail.com>
2026-02-01 14:53:02 +0000

    feat: Nível Mestre. Add matrizes de habilidades trianguo, cruz e octaedro. Add padrão para utilizar no tabuleiro e mudar de 3 para 5 onde havia navios.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define LINHASTABULEIRO 10
 #define COLUNASTABULEIRO 10
@@ -25,7 +26,7 @@ int main() {
         tabuleiro[i+2][1] = 3;
     }
     
-    //3.2- navio horizontal em C5{(5,2), (5,3), (5,4)}
+    //3.2- navio horizontal em C5-D5-E5 {(5,2), (5,3), (5,4)}
     for (int i = 0; i < TAMANHONAVIO; i++)
     {
         tabuleiro[4][i+2] = 3;
@@ -75,6 +76,73 @@ int main() {
         }
     }
      
+    
+    // Nível Mestre - Habilidades Especiais com Matrizes
+    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
+    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    // Exemplos de exibição das habilidades:
+    // Exemplo para habilidade em cone:
+    // 0 0 1 0 0
+    // 0 1 1 1 0
+    // 1 1 1 1 1
+
+    // Exemplo para habilidade em octaedro:
+    // 0 0 1 0 0
+    // 0 1 1 1 0
+    // 0 0 1 0 0
+    // Exemplo para habilidade em cruz:
+    // 0 0 1 0 0
+    // 1 1 1 1 1
+    // 0 0 1 0 0
+
+    // 1- criar 3 matrizes de habilidades
+    int centro = 2; //centro das habilidades
+    //1.1- CONE
+    int cone[3][5];
+    for (int i=0; i<3; i++){
+        for (int j=0; j<5; j++){
+            //formula para cone = abs(j-centro) <= i && i<3
+            if (abs(j-centro) <= i && i<3){
+                cone[i][j] = 1;
+            } 
+            else {
+                cone[i][j] = 0;
+            }
+        } 
+    }
+
+    //1.2- CRUZ
+    int cruz[5][5];
+    for (int i=0; i<5; i++){
+        for (int j=0; j<5; j++){
+            //formula para cruz = i == centro || j == centro
+            if (i == centro || j == centro){
+                cruz[i][j] = 1;
+            } 
+            else {
+                cruz[i][j] = 0;
+            }
+        } 
+    }
+    
+    
+    //1.3- OCTAEDRO
+    int octaedro[5][5];
+    int raioDoOctaedro = 2;
+    for (int i=0; i<5; i++){
+        for (int j=0; j<5; j++){
+            //formula para octaedro = abs(i - centro) + abs(j - centro) <= raioDoOctaedro
+            if (abs(i - centro) + abs(j - centro) <= raioDoOctaedro){
+                octaedro[i][j] = 1;
+            } 
+            else {
+                octaedro[i][j] = 0;
+            }
+        } 
+    }
+    
+    //2- exibir tabuleiro APENAS NAVIOS
+    printf("===== TABULEIRO COM NAVIOS ===== \n");
     //2.1- imprimindo cabeçalho
     printf("-- ");
     for (int i=0; i<COLUNASTABULEIRO; i++)
@@ -82,7 +150,7 @@ int main() {
         printf("%c ", cabeçalho[i]);
     }
     printf("\n");
-
+    
     //2.2- imiprimindo tabuleiro
     for (int i=0; i<LINHASTABULEIRO; i++)
     {
@@ -95,27 +163,155 @@ int main() {
         }
         printf("\n");
     }
-    
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    //3- integrar habilidades no tabuleiro
+    int coneL = 3;
+    int coneC = 1;
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (cone[i][j] == 1) {
+                int l = coneL + i;
+                int c = coneC + j - centro;
+                
+                if (l >= 0 && l < LINHASTABULEIRO &&
+                    c >= 0 && c < COLUNASTABULEIRO &&
+                    tabuleiro[l][c] == 0) {
+                        tabuleiro[l][c] = 5;
+                    }
+                }
+            }
+        }
+        
+    //3.1- CONE em B4 -> (linha 3, coluna 1)
+    printf("\n===== TABULEIRO COM HABILIDADES ===== \n");
+    printf("===== CONE EM B4 ===== \n");
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    //3.1.1- imprimindo cabeçalho
+    printf("-- ");
+    for (int i=0; i<COLUNASTABULEIRO; i++)
+    {
+        printf("%c ", cabeçalho[i]);
+    }
+    printf("\n");
+
+    //3.1.2- imiprimindo tabuleiro
+    for (int i=0; i<LINHASTABULEIRO; i++)
+    {
+        //if-else para que a numeração das linhas fique estetica, com 2 espaços de 1-9 e um espaço na linha 10
+        if(i==9){ printf("%d ", i+1);}
+        else {printf("%d  ", i+1);}
+        for (int j=0; j<COLUNASTABULEIRO; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    //3.2- CRUZ em D6 -> (linha 5, coluna 3)
+    int cruzL = 5;
+    int cruzC = 3;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (cruz[i][j] == 1) {
+                int l = cruzL + i - centro;
+                int c = cruzC + j - centro;
+
+                if (l >= 0 && l < LINHASTABULEIRO &&
+                    c >= 0 && c < COLUNASTABULEIRO &&
+                    tabuleiro[l][c] == 0) {
+                    tabuleiro[l][c] = 5;
+                }
+            }
+        }
+    }
+    
+    printf("\n===== CRUZ EM D6 ===== \n");
+    //3.2.1- imprimindo cabeçalho
+    printf("-- ");
+    for (int i=0; i<COLUNASTABULEIRO; i++)
+    {
+        printf("%c ", cabeçalho[i]);
+    }
+    printf("\n");
+    
+    //3.2.2- imiprimindo tabuleiro
+    for (int i=0; i<LINHASTABULEIRO; i++)
+    {
+        //if-else para que a numeração das linhas fique estetica, com 2 espaços de 1-9 e um espaço na linha 10
+        if(i==9){ printf("%d ", i+1);}
+        else {printf("%d  ", i+1);}
+        for (int j=0; j<COLUNASTABULEIRO; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    
+    //3.3- OCTAEDRO em E4 -> (linha 3, coluna 4)
+    int octL = 3;
+    int octC = 4;
+    
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (octaedro[i][j] == 1) {
+                int l = octL + i - centro;
+                int c = octC + j - centro;
+                
+                if (l >= 0 && l < LINHASTABULEIRO &&
+                    c >= 0 && c < COLUNASTABULEIRO &&
+                    tabuleiro[l][c] == 0) {
+                    tabuleiro[l][c] = 5;
+                }
+            }
+        }
+    }
+    
+    printf("\n===== OCTAEDRO EM E4 ===== \n");
+    //3.2.1- imprimindo cabeçalho
+    printf("-- ");
+    for (int i=0; i<COLUNASTABULEIRO; i++)
+    {
+        printf("%c ", cabeçalho[i]);
+    }
+    printf("\n");
+    
+    //3.2.2- imiprimindo tabuleiro
+    for (int i=0; i<LINHASTABULEIRO; i++)
+    {
+        //if-else para que a numeração das linhas fique estetica, com 2 espaços de 1-9 e um espaço na linha 10
+        if(i==9){ printf("%d ", i+1);}
+        else {printf("%d  ", i+1);}
+        for (int j=0; j<COLUNASTABULEIRO; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    
+    //4- exibir tabuleiro DEPOIS DAS HABILIDADES
+    //4.1- imprimindo cabeçalho
+    printf("\n\n===== TABULEIRO DEPOIS DAS HABILIDADES ===== \n");
+    printf("-- ");
+    for (int i=0; i<COLUNASTABULEIRO; i++)
+    {
+        printf("%c ", cabeçalho[i]);
+    }
+    printf("\n");
+
+    //4.2- imiprimindo tabuleiro
+    for (int i=0; i<LINHASTABULEIRO; i++)
+    {
+        //if-else para que a numeração das linhas fique estetica, com 2 espaços de 1-9 e um espaço na linha 10
+        if(i==9){ printf("%d ", i+1);}
+        else {printf("%d  ", i+1);}
+        for (int j=0; j<COLUNASTABULEIRO; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
